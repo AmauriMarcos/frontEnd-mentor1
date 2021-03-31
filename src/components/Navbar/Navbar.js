a0ebcdd9efdfed90ecdb8d73d6cce376131e243f
AmauriMarcos <amaurisantos.m@gmail.com>
2021-03-31 17:22:27 +0200

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Navbar = () =>{
+    return(
+        <nav>
+            <div>
+                <p>loopstudios</p>
+            </div>
+            <ul>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Careers</a>
+                </li>
+                <li>
+                    <a href="#">Events</a>
+                </li>
+                <li>
+                    <a href="#">Products</a>
+                </li>
+                <li>
+                    <a href="#">Support</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
